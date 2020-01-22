4a5ce61a586e0b53f5b4063131ec034561bf6bc6
Tom Coemans <tom.coemans@gmail.com>
2020-01-22 09:13:24 +0100

    test aangepast

--- test/e2e/typography.test.js
+++ test/e2e/typography.test.js
@@ -7,10 +7,22 @@ describe('vl-typography', async () => {
     before(() => {
         return vlTypographyPage.load();
     });
-    
-    it('Als gebruiker kan ik de inhoud van een vl-typography zien', async () => {
-    	const vlTypography = await vlTypographyPage.getTypographyPTag();
-    	assert.eventually.include(vlTypography.getInnerHTML(), "<p>Lorem ipsum dolor sit amet");
+
+    it('als gebruiker kan ik de inhoud van een vl-typography zien', async () => {
+        const text = `
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <a href="#">tempor
+                        incididunt</a> ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                    nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.</p>
+                <p>Lorem dolor sit amet, consectetur adipisicing elit. Deleniti, in.</p>
+        `;
+        const vlTypography = await vlTypographyPage.getTypographyPTag();
+        const innerHTML = await vlTypography.getInnerHTML();
+        assert.equal(innerHTML.trim(), text.trim());
     });
 
 });
