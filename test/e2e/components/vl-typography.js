3def4b5fb9acc5ac071186c77bf9c23bf1d5ecbc
Tom Coemans <tom.coemans@gmail.com>
2020-01-22 12:47:29 +0100

    tekst functie overschreven

--- test/e2e/components/vl-typography.js
+++ test/e2e/components/vl-typography.js
@@ -1,5 +1,9 @@
 const { VlElement } = require('vl-ui-core');
 
-class VlTypography extends VlElement { }
+class VlTypography extends VlElement {
+    async getText() {
+        return this.shadowRoot.getText();
+    }
+}
 
 module.exports = VlTypography;
