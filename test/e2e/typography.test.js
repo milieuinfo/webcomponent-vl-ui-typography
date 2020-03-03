d92648091f34e4d3db9eda62b445cf777c81ed22
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-03-03 15:34:55 +0100

    UIG-387 assert eventually gebruiken

--- test/e2e/typography.test.js
+++ test/e2e/typography.test.js
@@ -23,7 +23,7 @@ describe('vl-typography', async () => {
         const vlTypography = await vlTypographyPage.getTypographyPTag();
         const innerHTML = await vlTypography.getInnerHTML();
         assert.equal(innerHTML.trim(), text.trim());
-        assert.equal(await vlTypography.getText(), "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem dolor sit amet, consectetur adipisicing elit. Deleniti, in.");
+        await assert.eventually.equal(vlTypography.getText(), "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem dolor sit amet, consectetur adipisicing elit. Deleniti, in.");
     });
 
 });
